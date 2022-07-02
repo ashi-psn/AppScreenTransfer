6f4a79c9c8123f1ca76d27fff8e66e0c34689120
ashi-psn <psn.arcz.vtzf@gmail.com>
2022-07-02 15:29:35 +0900

    Initial Commit

---
+++ AppScreenTransfer/AppScreenTransferApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppScreenTransferApp.swift
+//  AppScreenTransfer
+//
+//  Created by ashizawa on 2022/07/02.
+//
+
+import SwiftUI
+
+@main
+struct AppScreenTransferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
